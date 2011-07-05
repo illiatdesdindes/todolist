89d3f0d434e948431ad44f17072984e46cba001b
illiatdesdindes <illiatdesdindes@gmail.com>
2011-07-05 23:38:09 +0200

    first commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,28 @@
+class ListsController < ApplicationController
+  
+  respond_to :html, :xml, :js
+  
+  def index
+    respond_with( @lists = List.all)
+  end
+  
+  def new
+    @list = List.new
+  end
+  
+  def create
+    @list = List.new(params[:list])
+    
+    if @list.save
+      flash[:notice] = "List created."
+      redirect_to list_path(@list)
+    else
+      #une seule ligne au lieu de deux: le flash est le deuxieme argument 
+      redirect_to new_list_path, :error => "Could not create list"
+    end
+  end
+  
+  def show
+    @list = List.find(params[:id])
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = 'd53c821cef4b023312e38519c5dca0034c8302543913061210cec86dd40ecd7b6a9036ad84bc5dbe615c6c3578608e8313243e0f0381216154bf69490cc204fc'
